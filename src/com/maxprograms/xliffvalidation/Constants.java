4cee374d4871012fa689a4d4bdd34a3416f20247
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2021-02-16 09:59:38 -0300

    Updated build number

--- src/com/maxprograms/xliffvalidation/Constants.java
+++ src/com/maxprograms/xliffvalidation/Constants.java
@@ -22,6 +22,6 @@ public class Constants {
 	public static final String STATUS = "status";
 	public static final String REASON = "reason";
 	public static final String VERSION = "1.0.0";
-	public static final String BUILD = "202101_0750";
+	public static final String BUILD = "20210216_0959";
 	public static final String OK = "OK";
 }
