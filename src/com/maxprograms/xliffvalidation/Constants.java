8720be96689f3ab4efe06b7d39f8f5a20028793b
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2021-05-12 11:10:37 -0300

    Updated OpenXLIFF build

--- src/com/maxprograms/xliffvalidation/Constants.java
+++ src/com/maxprograms/xliffvalidation/Constants.java
@@ -22,6 +22,6 @@ public class Constants {
 	public static final String STATUS = "status";
 	public static final String REASON = "reason";
 	public static final String VERSION = "1.0.0";
-	public static final String BUILD = "20210417_1046";
+	public static final String BUILD = "20210512_1108";
 	public static final String OK = "OK";
 }
